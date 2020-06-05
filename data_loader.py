0b435f7a4c087533d9638d2302016a00024c478d
zhaofeng-shu33 <616545598@qq.com>
2020-06-05 21:58:03 +0800

    limit num

--- data_loader.py
+++ data_loader.py
@@ -1,5 +1,5 @@
 import numpy as np
-def load_data():
+def load_data(limit_num=300):
     X_train = []
     X_test = []
     with open('build/supplemental_data.txt') as f:
@@ -16,7 +16,7 @@ def load_data():
             X_train.append(value_list[:63])
             X_test.append(value_list[-25:])
             st = f.readline()
-    return (np.asarray(X_train), np.asarray(X_test))
+    return (np.asarray(X_train[:limit_num]), np.asarray(X_test[:limit_num]))
 
 if __name__ == '__main__':
     load_data()
