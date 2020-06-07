e473d24fd6627549df2b69963200fda9e2c234e9
zhaofeng-shu33 <616545598@qq.com>
2020-06-07 09:35:37 +0800

    add plot routine

--- experiment.py
+++ experiment.py
@@ -1,5 +1,5 @@
 import argparse
-
+import re
 from scipy.cluster.hierarchy import linkage
 from info_cluster import InfoCluster
 from ete3 import Tree
@@ -10,11 +10,14 @@ from utility import scipy_linkage_obj_to_ete3_tree
 from utility import create_model
 from utility import convert_bhc_tree_to_ete_tree
 
+from matplotlib import pyplot as plt
+
+GRID = [100,200,300,400,500,600]
 
 def brt(data):
     # Hyper-parameters (these values must be optimized!)
     g = 10
-    scalling_factor = 0.001
+    scalling_factor = 0.01
     alpha = 0.5
 
     model = create_model(data, g, scalling_factor)
@@ -47,20 +50,44 @@ def ic(data, metric='rbf'):
 def compute_score(alg, train_data, test_data):
     tree_1 = alg(train_data)
     tree_2 = alg(test_data)
-    res = tree_1.compare(tree_2, unrooted=True)
+    if len(tree_1.children) == train_data.shape[0] \
+        and len(tree_2.children) == train_data.shape[0]:
+            print('Warning: trivial trees for n = %d' % train_data.shape[0])
+    res = tree_1.compare(tree_2, unrooted=True)    
     metric_score = res['norm_rf']
     return metric_score
 
+def run_all():
+    X_train, X_test = load_data(600)
+    result_dic = {}
+    for method in [ic, ahc]:
+        method_name = re.search('function ([a-z]+) at', str(method)).group(1)
+        result_dic[method_name] = []     
+        for i in GRID:
+            score = compute_score(method, X_train[:i,:], X_test[:i,:])
+            result_dic[method_name].append(score)
+    return result_dic
+
+def plot_results(result_dic):
+    for k,v in result_dic.items():
+        plt.scatter(GRID, v, labels=k)
+    plt.savefig('build/plot_results.eps')
+
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
+    parser.add_argument('--task', default='single', choices=['single', 'all'])
     parser.add_argument('--method', default='ic', choices=['ic', 'ahc', 'brt'])
     parser.add_argument('--limit_num', default=600, type=int, help="how many samples to use")
     args = parser.parse_args()
-    X_train, X_test = load_data(args.limit_num)
-    if args.method == 'ic':
-        score = compute_score(ic, X_train, X_test)
-    elif args.method == 'ahc':
-        score = compute_score(ahc, X_train, X_test)
-    elif args.method == 'brt':
-        score = compute_score(brt, X_train, X_test)
-    print(score)
+    if args.task == 'single':
+        X_train, X_test = load_data(args.limit_num)
+        if args.method == 'ic':
+            score = compute_score(ic, X_train, X_test)
+        elif args.method == 'ahc':
+            score = compute_score(ahc, X_train, X_test)
+        elif args.method == 'brt':
+            score = compute_score(brt, X_train, X_test)
+        print(score)
+    elif args.task == 'all':
+        result_dic = run_all()
+        plot_results(result_dic)
