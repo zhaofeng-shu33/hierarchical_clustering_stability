ad3c8d80ff7b0069b8fdeb673844fae20b3cf3d2
zhaofeng-shu33 <616545598@qq.com>
2020-06-07 11:32:35 +0800

    add non binary test

--- test.py
+++ test.py
@@ -25,6 +25,11 @@ class TreeCompare(unittest.TestCase):
         t_2 = Tree('((a,c),b);')
         result = robinson_foulds(t_1, t_2)
         self.assertAlmostEqual(result['norm_rf'], 1.0)
+    def test_non_binary_tree_compare(self):
+        t_1 = Tree('((a,b,d),c);')
+        t_2 = Tree('(((a,b),d),c);')
+        result = robinson_foulds(t_1, t_2)
+        self.assertAlmostEqual(result['norm_rf'], 1/3.0)
 
 if __name__ == '__main__':
     unittest.main()
