4a7e638b980b6647f07bb0fb2d932681bdcb9fe2
zhaofeng-shu33 <616545598@qq.com>
2022-02-12 07:39:54 +0800

    add back data

--- data_loader.py
+++ data_loader.py
@@ -1,21 +1,18 @@
 import numpy as np
+import csv
+
+def read_data(file_name):
+    with open(file_name) as cf:
+        r = csv.reader(cf, delimiter=',')
+        next(r) # skip header
+        data = []
+        for row in r:
+            data.append([float(i) for i in row[1:]])
+        return data
+
 def load_data(limit_num=600):
-    X_train = []
-    X_test = []
-    with open('build/supplemental_data.txt') as f:
-        # abondon the first three lines
-        f.readline()
-        f.readline()
-        f.readline()
-        st = f.readline()
-        while st != '':
-            value_list = st.rstrip().split('\t')[2:]
-            value_list = [float(i) for i in value_list]
-            if len(value_list) == 0:
-                break
-            X_train.append(value_list[:63])
-            X_test.append(value_list[-25:])
-            st = f.readline()
+    X_train = read_data('train.csv')
+    X_test = read_data('test.csv')
     return (np.asarray(X_train[:limit_num]), np.asarray(X_test[:limit_num]))
 
 if __name__ == '__main__':
